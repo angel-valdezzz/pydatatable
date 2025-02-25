144868734af71afc095ee2b3838d3b8cc0681e68
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2025-02-25 07:50:42 -0600

    Definicion de errores

---
+++ src/pydatatable/utils/errors.py
@@ -0,0 +1,17 @@
+class PyDataTableError(Exception):
+    """PyDataTableError"""
+
+class FileNotFoundException(PyDataTableError):
+    """FileNotFoundException"""
+
+class FileReadingException(PyDataTableError):
+    """FileReadingException"""
+
+class FileWritingException(PyDataTableError):
+    """FileWritingException"""
+
+class FileExtensionException(PyDataTableError):
+    """FileReadingException"""
+
+class SheetNameHasNotEmptyException(PyDataTableError):
+    """SheetNameHasNotEmptyException"""
