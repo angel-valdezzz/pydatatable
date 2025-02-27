8b9c91b567e8d44b5dc043427ac972adb91600e2
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2025-02-27 07:40:55 -0600

    Documenta las clases publicas

--- src/pydatatable/core/datatable.py
+++ src/pydatatable/core/datatable.py
@@ -3,7 +3,13 @@ from pydatatable.utils.observer import FieldChangeObserver
 from pydatatable.core.dt_header import DTHeader
 
 class DataTable:
-    """TestData"""
+    """Representa un conjunto de datos tabulares (filas y columnas).
+
+    Cada fila es un objeto DTRow y cada columna es un objeto DTField.
+    La primer fila es considerada como la fila de encabezados.
+    Aunque la fila 0 es de encabezados, no se considera como una fila de datos y no se incluye en el conteo de filas, por lo que se puede acceder a la primer fila de datos con el indice 0.
+    """
+
     def __init__(self, rows: list[DTRow], observer: FieldChangeObserver):
         self._rows = rows
         self._len = len(rows)
@@ -13,11 +19,11 @@ class DataTable:
         return len(self._rows)
 
     def row(self, index: int) -> DTRow:
-        """row"""
+        """Obtiene una fila por su indice."""
         return self._rows[index]
 
     def total_rows(self):
-        """total_rows"""
+        """Indica el total de filas de datos."""
         return self._len
 
     def __getitem__(self, index: int):
@@ -27,12 +33,12 @@ class DataTable:
         return iter(self._rows)
 
     def headers(self):
-        """headers"""
+        """Obtiene los encabezados de las columnas."""
         return list(
             {DTHeader(field.name, field.index) for row in self._rows for field in row}
               | {DTHeader(event["field"].name, event["pos"]) for event in self._observer.events}
         )
 
     def to_dict(self):
-        """to_dict"""
+        """Convierte el DataTable a una lista de diccionarios."""
         return [row.to_dict() for row in self._rows]
